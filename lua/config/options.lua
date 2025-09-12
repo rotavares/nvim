113b77680241d61c34faaa41f0405bc7a9433d15
Rodrigo Tavares <rotavares@proton.me>
2025-09-12 14:22:30 -0300

    aaaa

---
+++ lua/config/options.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.mouse = "a"
+
+vim.o.clipboard = "unnamedplus"
+
+vim.opt.undofile = true
+
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
+vim.o.breakindent = true
